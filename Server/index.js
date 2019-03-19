7ad7071198e62ae636e8c435bc7f9bc3b322f6f2
jn-robert <jeannoel.robert70@gmail.com>
2019-03-19 01:05:35 +0100

    Ajout du jeu / calcul du score / identification / ajout de lieux / gestion distance / gestion temps

--- Server/index.js
+++ Server/index.js
@@ -24,44 +24,150 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../Client/Home.html'));
 });
 
+app.get('/add', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Client/Add.html'));
+});
+
 let listener = socket.listen(server, {log: false});
 
-
-let position = [];
 let tab;
-let distance = 1;
-
+let coordServ = [];
+let coordRecu = [];
+let score = 0;
+let D = 0.001;
+let temp = 0;
 
 function start(socket) {
+
     socket.on('Start', function () {
         con.query('SELECT * FROM location', (err, rows) => {
             if (err) throw err;
 
             tab = rows;
+            tab = shuffle(tab);
+
+            if (tab.length > 10) {
+                let tmpTab = [];
+                for (let x = 0; x < 10; x++) {
+                    tmpTab.push(tab[x]);
+                }
+                tab = tmpTab;
+            }
+
+            getCoordBdd();
+
             socket.emit('Partie', {
-                tab: rows
+                tab: tab
             });
         })
     });
 
     socket.on('clicMap', (data) => {
-        console.log(data.latitude);
-        console.log(data.longitude);
+        let coord = [];
+        coord.push(data.latitude);
+        coord.push(data.longitude);
+        coordRecu.push(coord);
+        calculScore();
+        temp = data.temp;
     });
 
     socket.on('End', function () {
-       calculScore();
+
+    });
+
+    socket.on('Login', (data) => {
+        let dataUser = [];
+        con.query('SELECT * FROM user', (err, rows) => {
+            if (err) throw err;
+            dataUser = rows;
+
+            for (let x = 0; x < dataUser.length; x++) {
+                if (data.dataUser[0] === dataUser[x].pseudo && data.dataUser[1] === dataUser[x].mdp) {
+                    socket.emit('LoginVerif', {
+                        token: data.dataUser[2]
+                    });
+                    break
+                }
+                else if (x + 1 === dataUser.length) {
+                    socket.emit('BadLogin', {
+                        token: data.dataUser[2]
+                    });
+                }
+            }
+        });
+    });
+
+    socket.on('NewData', (data) => {
+        let sql = "INSERT INTO `location` (`id`, `titre`, `lien`, `latitude`, `longitude`) VALUES (NULL,";
+        let valueSql = " '"+data.newPlace[0]+"','"+data.newPlace[1]+"','"+data.newPlace[2]+"','"+data.newPlace[3]+"'";
+        let cmd = sql + valueSql + ");";
+        con.query(cmd, function (err, rows) {
+            if (err) throw err;
+        });
     });
 
 
 }
 
 function calculScore() {
-    for (let x = 0; x < tab.length; x++){
+    score = 0;
+    let coef = 0;
+
+    if (temp < 2) {
+        coef = 4;
+    }
+    else if (temp < 5) {
+        coef = 2
+    }
+
+    for (let x = 0; x < coordRecu.length; x++) {
+        let distance = 0;
+        distance = Math.sqrt(Math.pow((coordServ[x][0] - coordRecu[x][0]), 2) + Math.pow((coordServ[x][1] - coordRecu[x][1]), 2));
+        if (distance < D) {
+            score += 5 * coef;
+        }
+        else if (distance < D * 2) {
+            score += 3 * coef;
+        }
+        else if (distance < D * 3) {
+            score += coef;
+        }
+    }
+    console.log(score);
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function getCoordBdd() {
+    coordServ = [];
+    coordRecu = [];
+    for (let x = 0; x < tab.length; x++) {
+        let coord = [];
+        coord.push(tab[x].latitude);
+        coord.push(tab[x].longitude);
+        coordServ.push(coord);
     }
 }
 
 listener.sockets.on('connection', function (socket) {
     start(socket);
 });
+
 server.listen(8888);
--- bdd.sql
+++ bdd.sql
@@ -2,9 +2,22 @@ CREATE TABLE location(
  id INT AUTO_INCREMENT ,
  titre VARCHAR(20),
  lien VARCHAR(100),
- latitude INT,
- longitude INT,
+ latitude FLOAT(10,7),
+ longitude FLOAT(10,7),
  PRIMARY KEY(id)
 );
 
+CREATE TABLE user(
+ id INT AUTO_INCREMENT,
+ pseudo VARCHAR(20),
+ mdp VARCHAR(20),
+ PRIMARY KEY(id)
+);
+
+INSERT INTO `location` (`id`, `titre`, `lien`, `latitude`, `longitude`) VALUES (NULL, 'Iut Belfort', 'http://actu.univ-fcomte.fr/sites/default/files/batiment-iut-90-0111.jpg', '47.6437703', '6.8384994');
 INSERT INTO `location` (`id`, `titre`, `lien`, `latitude`, `longitude`) VALUES (NULL, 'La Poudrière', 'https://www.jds.fr/medias/image/la-poudriere-rockhatry-belfort-7378-600-600-F.jpg', '47.6373541', '6.863235');
+INSERT INTO `user` (`id`, `pseudo`, `mdp`) VALUES (NULL, 'test', 'test'), (NULL, 'admin', 'admin');
+
+
+
+
